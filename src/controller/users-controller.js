f34cedd5e433cfeb8719449d14011b842adde7a6
junhui324 <junhuijang00@gmail.com>
2023-04-26 20:18:01 +0900

    :bug: 헤더에 cookie를 넣어서 보낼 수 없는 문제 발생
    authorization에 넣어서 보내는 방법 추가

--- src/controller/users-controller.js
+++ src/controller/users-controller.js
@@ -93,7 +93,8 @@ const usersController = {
   //사용자 정보 조회
   getProfile: async (req, res, next) => {
     try {
-      const accessToken = req.cookies.accessToken;
+      //const accessToken = req.cookies.accessToken;
+      const accessToken = req.headers.authorization.split('Bearer ')[1];
       const email = authServices.decodedAccessToken(accessToken);
       const { password } = req.body;
       const user = await usersService.getProfile(email, password);
